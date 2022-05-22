ad155618ae9f5a6d6d0538fa9da8392019ac2e19
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-05-21 20:16:59 -0600

    Added tentative Stored Procedures for check email and project name

---
+++ Planilla_UCR/DB_Planilla/StoredProcedures/Project/GetAllEmails.sql
@@ -0,0 +1,5 @@
+﻿CREATE Procedure EmailCheck(@UserEmail varchar(255))
+AS
+BEGIN
+    Select * from Employer where Employer.Email = @UserEmail
+END
---
+++ Planilla_UCR/DB_Planilla/StoredProcedures/Project/GetAllNameProjects.sql
@@ -0,0 +1,6 @@
+﻿
+CREATE Procedure ProjectNameCheck(@ProjectName varchar(255))
+AS
+BEGIN
+    Select * from Project where Project.ProjectName = @ProjectName
+END
