cb601e6ba4287584fa558988b049c1c886d38f3e
Jeremy Vargas Artavia <jeremychaman@gmail.com>
2022-06-13 17:00:09 -0600

    Mapping subscribes in Application Layer.

--- Planilla_UCR/Application/Subscribes/ISubscribeService.cs
+++ Planilla_UCR/Application/Subscribes/ISubscribeService.cs
@@ -1,10 +1,9 @@
 ﻿using Domain.Subscribes.Entities;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
-namespace Domain.Subscribes.Repositories
+namespace Application.Subscribes
 {
-    public interface ISubscribeRepository
+    public interface ISubscribeService
     {
         Task CreateSubscribeAsync(Subscribe subscription);
         Task<IEnumerable<Subscribe>> GetEmployeesBySubscription(string employerEmail, string projectName, string subscriptionName);
