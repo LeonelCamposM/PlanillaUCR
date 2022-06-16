8be96eb153a4bc9ac306900ee5b8ee798ffebee2
RMPV09 <mauupvillegas2000@hotmail.com>
2022-06-16 01:23:39 -0600

    Functional, cleaning code. Needs more cleaning.

---
+++ Planilla_UCR/Presentation/Agreements/Models/JoinedTables.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Presentation.Agreements.Models
+{
+    internal class JoinedTables
+    {
+        public string EmployerEmail { get; set; }
+        public string ProjectName { get; set; }
+        public string PaymentInterval { get; set; }
+        public string EmployeeEmail { get; set; }
+        public string ContractType { get; set; }
+        public DateTime ContractStartDate { get; set; }
+        public DateTime ContractFinishDate { get; set; }
+        public int MountPerHour { get; set; }
+
+        public JoinedTables() { 
+        }
+
+        public JoinedTables(string EmployerEmail, string ProjectName, string PaymentInterval, string EmployeeEmail, string ContractType, DateTime ContractStartDate, DateTime ContractFinishDate, int MountPerHour)
+        {
+            this.EmployerEmail = EmployerEmail;
+            this.ProjectName = ProjectName;
+            this.PaymentInterval = PaymentInterval; 
+            this.EmployeeEmail = EmployeeEmail;
+            this.ContractType = ContractType;
+            this.ContractStartDate = ContractStartDate;
+            this.ContractFinishDate = ContractFinishDate;
+            this.MountPerHour = MountPerHour;
+        }
+    }
+}
