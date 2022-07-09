a1bbbda49f12de623d4e78dbefc0dcb94cccf55e
LeonelCamposM <leonelscamposm@gmail.com>
2022-07-08 20:46:56 -0600

    fix legal deduction calculator

--- Planilla_UCR/TestProject1/Infrastructure/TaxCalculatorTest.cs
+++ Planilla_UCR/TestProject1/Infrastructure/TaxCalculatorTest.cs
@@ -1,35 +1,115 @@
 ﻿using FluentAssertions;
 using Xunit;
-using Infrastructure.TaxCalculator.Repositories;
+using Application.TaxCalculator.Implementations;
 
 namespace Tests.Infrastructure
 {
     public class TaxCalculatorTest
     {
-        int _grossSalary = 863000;
-        string _taxName = "CCSS";
-        double _rentTax = 0;
-        double _ccssTax = 10.5;
-        double _taxPercentage = 10.5;
-        double _taxAmount = 90615;
+        TaxCalculatorService taxCalculatorService = new TaxCalculatorService();
+        string taxCCSS = "CCSS";
+        string taxRent = "Rent";
 
         [Fact]
-        public void GetPaymentTaxes()
+        public void GetNoRentTaxes()
         {
             //arrange
-            var taxCalculatorService = new TaxCalculatorRepository();
+            double _grossSalary = 200000.00;
+            double expectedCCSSAmount = 21000.00;
+            double expectedRentAmount = 0.00;
 
             //act
-            var rentTaxt = taxCalculatorService.GetRentTax(_grossSalary);
-            var ccsTax = taxCalculatorService.GetCSSTax(); 
-            var taxPercentaje = taxCalculatorService.GetTaxPercentage(_taxName, _grossSalary);
-            var taxAmount = taxCalculatorService.GetTaxAmount(_taxName, _grossSalary);
+            double CCSSAmount = taxCalculatorService.GetTaxAmount(taxCCSS, _grossSalary);
+            double RentAmount = taxCalculatorService.GetTaxAmount(taxRent, _grossSalary);
 
             //assert
-            rentTaxt.Should().Be(_rentTax);
-            ccsTax.Should().Be(_ccssTax);
-            taxPercentaje.Should().Be(_taxPercentage);  
-            taxAmount.Should().Be(_taxAmount);
+            CCSSAmount.Should().Be(expectedCCSSAmount);
+            RentAmount.Should().Be(expectedRentAmount);
+        }
+
+        [Fact]
+        public void GetOverF1RentTaxes()
+        {
+            //arrange
+            double _grossSalary = 864000.00;
+            double expectedCCSSAmount = 90720.00;
+            double expectedRentAmount = 100.00;
+
+            //act
+            double CCSSAmount = taxCalculatorService.GetTaxAmount(taxCCSS, _grossSalary);
+            double RentAmount = taxCalculatorService.GetTaxAmount(taxRent, _grossSalary);
+
+            //assert
+            CCSSAmount.Should().Be(expectedCCSSAmount);
+            RentAmount.Should().Be(expectedRentAmount);
+        }
+
+        [Fact]
+        public void GetF2RentTaxes()
+        {
+            //arrange
+            double _grossSalary = 2223000.00;
+            double expectedCCSSAmount = 233415.00;
+            double expectedRentAmount = 183800.00;
+
+            //act
+            double CCSSAmount = taxCalculatorService.GetTaxAmount(taxCCSS, _grossSalary);
+            double RentAmount = taxCalculatorService.GetTaxAmount(taxRent, _grossSalary);
+
+            //assert
+            CCSSAmount.Should().Be(expectedCCSSAmount);
+            RentAmount.Should().Be(expectedRentAmount);
+        }
+
+        [Fact]
+        public void GetOverF3RentTaxes()
+        {
+            //arrange
+            double _grossSalary = 2224000.00;
+            double expectedCCSSAmount = 233520.00;
+            double expectedRentAmount = 184000.00;
+
+            //act
+            double CCSSAmount = taxCalculatorService.GetTaxAmount(taxCCSS, _grossSalary);
+            double RentAmount = taxCalculatorService.GetTaxAmount(taxRent, _grossSalary);
+
+            //assert
+            CCSSAmount.Should().Be(expectedCCSSAmount);
+            RentAmount.Should().Be(expectedRentAmount);
+        }
+
+        [Fact]
+        public void GetF4RentTaxes()
+        {
+            //arrange
+            double _grossSalary = 4445000.00;
+            double expectedCCSSAmount = 466725.00;
+            double expectedRentAmount = 628200.00;
+
+            //act
+            double CCSSAmount = taxCalculatorService.GetTaxAmount(taxCCSS, _grossSalary);
+            double RentAmount = taxCalculatorService.GetTaxAmount(taxRent, _grossSalary);
+
+            //assert
+            CCSSAmount.Should().Be(expectedCCSSAmount);
+            RentAmount.Should().Be(expectedRentAmount);
+        }
+
+        [Fact]
+        public void GetOverF4RentTaxes()
+        {
+            //arrange
+            double _grossSalary = 5445000.00;
+            double expectedCCSSAmount = 571725.00;
+            double expectedRentAmount = 878200.00;
+
+            //act
+            double CCSSAmount = taxCalculatorService.GetTaxAmount(taxCCSS, _grossSalary);
+            double RentAmount = taxCalculatorService.GetTaxAmount(taxRent, _grossSalary);
+
+            //assert
+            CCSSAmount.Should().Be(expectedCCSSAmount);
+            RentAmount.Should().Be(expectedRentAmount);
         }
     }
 }
