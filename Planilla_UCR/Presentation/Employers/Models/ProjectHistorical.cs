b6073d0228147faffc8c84661b48be51f73723a0
Jeremy Vargas Artavia <jeremychaman@gmail.com>
2022-07-11 17:43:58 -0600

    Navigation Menu changed. New Page for report # 3 added.

---
+++ Planilla_UCR/Presentation/Employers/Models/ProjectHistorical.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Presentation.Employers.Models
+{
+    internal class ProjectHistorical
+    {
+
+        public string ProjectName { get; set; }
+        public string PaymentFrecuency { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public double GrossSalary { get; set; }
+        public double Benefits { get; set; }
+        public double EmployerCharges { get; set; }
+        public double ObligatoryDeductions { get; set; }
+        public double VoluntaryDeductions { get; set; }
+        public double EmployerCost { get; set; }
+
+        public ProjectHistorical()
+        {
+            ProjectName = String.Empty;
+            PaymentFrecuency = String.Empty;
+            PaymentDate = DateTime.Now;
+            GrossSalary = 0;
+            Benefits = 0;
+            EmployerCharges = 0;
+            ObligatoryDeductions = 0;
+            VoluntaryDeductions = 0;
+            EmployerCost = 0;
+        }
+
+        public ProjectHistorical(string projectName, string paymentFrecuency,
+            DateTime paymentDate, double grossSalary, double benefits, double employerCharges,
+            double obligatoryDeductions, double voluntaryDeductions, double employerCost)
+        {
+            ProjectName = projectName;
+            PaymentFrecuency = paymentFrecuency;
+            PaymentDate = paymentDate;
+            GrossSalary = grossSalary;
+            Benefits = benefits;
+            EmployerCharges = employerCharges;
+            ObligatoryDeductions = obligatoryDeductions;
+            VoluntaryDeductions = voluntaryDeductions;
+            EmployerCost = employerCost;
+        }
+    }
+}
