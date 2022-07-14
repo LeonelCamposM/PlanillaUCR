2880d3936a8be6df818b4e262ab15fab67ac2ea0
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-14 16:06:18 -0600

    Changes in the model foe employee list history

--- Planilla_UCR/Presentation/Employers/Models/EmployeeHistoryList.cs
+++ Planilla_UCR/Presentation/Employers/Models/EmployeeHistoryList.cs
@@ -8,6 +8,7 @@ namespace Presentation.Employers.Models
         public string EmployeeName { get; set; }
         public string EmployeeLastName1 { get; set; }
         public string EmployeeLastName2 { get; set; }
+        public string EmployeeEmail { get; set; }
         public string ProjectName { get; set; }
         public int Ssn { get; set; }
         public string ContractType { get; set; }
@@ -24,6 +25,7 @@ namespace Presentation.Employers.Models
             this.EmployeeName = "";
             this.EmployeeLastName1 = "";
             this.EmployeeLastName2 = "";
+            this.EmployeeEmail = "";
             this.ProjectName = "";
             this.Ssn = 0;
             this.ContractType = "";
@@ -39,11 +41,12 @@ namespace Presentation.Employers.Models
         public EmployeeHistoryList(string employeeName, string employeeLastName1, string employeeLastName2, string projectName, 
                                    string contractType, double grossSalary, double totalBenefits, double employerSocialCharges,
                                    double mandatoryEmployeeDeductions, double voluntaryDeductions,double employeeCost, int ssn,
-                                   string paymenDate)
+                                   string paymenDate, string employeeEmail)
         {
             this.EmployeeName = employeeName;
             this.EmployeeLastName1 = employeeLastName1;
             this.EmployeeLastName2 = employeeLastName2;
+            this.EmployeeEmail = employeeEmail;
             this.ProjectName = projectName;
             this.Ssn = ssn;
             this.ContractType = contractType;
