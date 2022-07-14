f23a2df3bf5d4f477de6306d7397f077cb1bdee9
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-13 18:27:05 -0600

    Create new models for the excel

---
+++ Planilla_UCR/Presentation/Employers/Models/Excel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+using Presentation.Employers.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Presentation.Employers.XLS
+{
+    public class Excel
+    {
+        public async Task GenerateExcelReport5Async(IJSRuntime js, EmployeeHistoryList[] data, string filename = "Reporte histórico del pago de mis empleados.xlsx")
+        {
+            var report5 = new HistoricalEmployeePaymentsExcel();
+            string XLSStream = report5.Edition(data);
+
+            await js.InvokeVoidAsync("jsSaveAsFile", filename, XLSStream);
+        }
+    }
+}
