e6d1f9ffe27ba7d9353b3d78dc7df7cb608e95dd
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-07-17 11:53:20 -0600

    Unit test

--- Planilla_UCR/TestProject1/Application/UpdateEmployerEmailServiceTest.cs
+++ Planilla_UCR/TestProject1/Application/UpdateEmployerEmailServiceTest.cs
@@ -1,19 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Application.People.Implementations;
-using Application.People;
-using Application.Employers.Implementations;
-using Application.Employers;
+﻿using Application.Employers.Implementations;
 using Moq;
 using Xunit;
-using System;
 //Proyectos
-using Domain.People.Repositories;
-using Domain.People.Entities;
 using Domain.Employers.Repositories;
-using Domain.Employers.Entities;
+
 
 namespace Tests.Application
 {
