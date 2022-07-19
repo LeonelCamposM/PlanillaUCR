c455229302f148c8a47040b8df3954675c1dc633
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-19 09:35:52 -0600

    Change email message

--- Planilla_UCR/Application/Email/Implementations/EmailServices.cs
+++ Planilla_UCR/Application/Email/Implementations/EmailServices.cs
@@ -152,7 +152,7 @@ namespace Application.Email.Implementations
             string htmlContent = "<section>" + "<div>" + "<header style = BACKGROUND-COLOR:#00695c>" + "<center>" + "<FONT SIZE=5 COLOR=#FFFFFF>" + "<strong>" + "PlanillaUCR" +
                 "</strong>" + "</FONT>" + "</center>" + "</div>" + "</header>" + "</section>" + "<br>" + "</br>" + "<section>" + "<div>" + "Señor(a): " + employeeName + "<br>" +
                 "</br>" + "</div>" + "</section>" + "<section>" + "<div>" + "Reciba un cordial saludo." + "<br>" + "</br>" + "</div>" + "</section>" + "<section>" + "<div>" +
-                "Yo," + " " + employerName + " a cargo de " + projectName + ", me dirijo a usted, para comunicarle que actualmente tenemos mayor cantidad de beneficios disponibles " +
+                "Yo," + " " + employerName + ", a cargo de " + projectName + "; me dirijo a usted para comunicarle que actualmente tenemos mayor cantidad de beneficios disponibles " +
                 "para nuestros trabajadores." + "</br>" + " </div>" +"</section >" + "<section>" + "<div>" + "Decidimos aumentar los beneficios como agradecimiento por su trabajo. " +
                 "Para poder hacer uso de estos beneficios puede dirigirse a nuestra página y seleccionar los beneficios de su preferencia." + "</br>" + " </div>" + "</section >" +
                 "<section>" + "<div>" + "Esperamos que disfrute y le sean de provecho estos nuevos servicios que la compañía ha puesto a su disposición." + "<br>" + "</br>" + "</div>" + "</section>" + 
@@ -174,11 +174,11 @@ namespace Application.Email.Implementations
             string htmlContent = "<section>" + "<div>" + "<header style = BACKGROUND-COLOR:#00695c>" + "<center>" + "<FONT SIZE=5 COLOR=#FFFFFF>" + "<strong>" + "PlanillaUCR" +
                 "</strong>" + "</FONT>" + "</center>" + "</div>" + "</header>" + "</section>" + "<br>" + "</br>" + "<section>" + "<div>" + "Señor(a): " + employeeName + "<br>" +
                 "</br>" + "</div>" + "</section>" + "<section>" + "<div>" + "Reciba un cordial saludo." + "<br>" + "</br>" + "</div>" + "</section>" + "<section>" + "<div>" +
-                "Yo," + " " + employerName + " a cargo de " + projectName + " me dirijo a usted, para comunicarle que lamentablemente hemos actualizado el proyecto y ahora " +
-                "tenemos menor cantidad de beneficios disponibles." + "</br>" + "</div>" + "</section>" +
-                "Nos disculpamos por los inconvenientes y le pedimos que por favor se dirija a nuestra página web y se desuscriba de alguno de los beneficios, esto para que se cumpla con " +
-                "el presupuesto máximo definido por la compañía, el tiempo límite para realizar esta acción es " + message +"" + "<br>" + "</br>" + "</div>" + "</section>" + 
-                "<section>" + "<div>" + "De antemano nos disculpamos por todos los inconvenientes que esto le pueda causar." + "<br>" + "</br>" + "</div>" + "</section>" +
+                "Yo," + " " + employerName + ", a cargo de " + projectName + "; me dirijo a usted para comunicarle que lamentablemente hemos actualizado el proyecto y ahora " +
+                "tenemos menor cantidad de beneficios disponibles." + "</br>" + "</div>" + "</section>" + "Nos disculpamos por los inconvenientes. Además, le pedimos que por " +
+                "favor se dirija a nuestra página web en la sección de beneficios y se desuscriba de alguno de ellos, esto para que se cumpla con el presupuesto máximo definido "+
+                "por la compañía, el tiempo límite para realizar esta acción es " + message +"" + "<br>" + "</br>" + "</div>" + "</section>" + "<section>" + "<div>" + 
+                "De antemano nos disculpamos por todos los inconvenientes que esto le pueda causar." + "<br>" + "</br>" + "</div>" + "</section>" +
                 "<section>" + "<div>" + "Nos despedimos de usted con un gran agradecimiento por su labor."+ "<br>" + "</br>" + "</div>" + "</section>" + "<section>" + "<div>" +
                 "Cordialmente, " + "<br>" + "</br>" + "</div>" + "</section>" + "<section>" + "<div>" + employerName + "<br>" + "</br>" + "</div>" + "</section>";
             string subject = "Cambio en beneficios";
@@ -197,7 +197,7 @@ namespace Application.Email.Implementations
                 "</strong>" + "</FONT>" + "</center>" + "</div>" + "</header>" + "</section>" + "<br>" + "</br>" + "<section>" + "<div>" + "Señor(a): " + employerName + "<br>" +
                 "</br>" + "</div>" + "</section>" + "<section>" + "<div>" + "En este correo se incluye la lista de empleados que actualmente están excediendo el monto y/o " +
                 "la cantidad de beneficios de " + projectName + ". " + "<br>" + "</br>" + "</div> " + "</section>";
-            htmlContent += File.ReadAllText("../Server_Planilla/wwwroot/emails/OverstaffedEmployees.html");
+            htmlContent += File.ReadAllText("../wwwroot/wwwroot/emails/OverstaffedEmployees.html");
             
             htmlContent += "<section>" + "<div>" +"<br>" + "</br>" + "A cada uno de ellos se les envió un correo indicando que deben desuscribirse de alguno de sus beneficios." +
                 "</div> " + "</section>" + "<section>" + "<div>" + "Favor monitorear que cumplan con la fecha límite." + "<br>" + "</br>" + "</div>" + "</section>";
