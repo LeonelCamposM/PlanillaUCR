ffd14d2dfd36515c6e738a4d9d735f9c8bebd73b
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-19 11:41:14 -0600

    Final details for pr

--- Planilla_UCR/Application/Email/Implementations/EmailServices.cs
+++ Planilla_UCR/Application/Email/Implementations/EmailServices.cs
@@ -154,7 +154,7 @@ namespace Application.Email.Implementations
                 "</br>" + "</div>" + "</section>" + "<section>" + "<div>" + "Reciba un cordial saludo." + "<br>" + "</br>" + "</div>" + "</section>" + "<section>" + "<div>" +
                 "Yo," + " " + employerName + ", a cargo de " + projectName + "; me dirijo a usted para comunicarle que actualmente tenemos mayor cantidad de beneficios disponibles " +
                 "para nuestros trabajadores." + "</br>" + " </div>" +"</section >" + "<section>" + "<div>" + "Decidimos aumentar los beneficios como agradecimiento por su trabajo. " +
-                "Para poder hacer uso de estos beneficios puede dirigirse a nuestra página y seleccionar los beneficios de su preferencia." + "</br>" + " </div>" + "</section >" +
+                "Para poder hacer uso de estos beneficios puede dirigirse a nuestra página, en la sección de beneficios y seleccionar los beneficios de su preferencia." + "</br>" + " </div>" + "</section >" +
                 "<section>" + "<div>" + "Esperamos que disfrute y le sean de provecho estos nuevos servicios que la compañía ha puesto a su disposición." + "<br>" + "</br>" + "</div>" + "</section>" + 
                 "<section>" + "<div>" + "Estamos muy agradecidos por sus grandes labores. Esperamos seguir creciendo y mejorando para nuestros trabajadores y clientes." + "</div>" + 
                 "<section>" + "<div>" + "Nos despedimos de usted con un gran agradecimiento." + "<br>" + "</br>" + "</div>" + "</section>" + "<section>" + "<div>" +
@@ -197,7 +197,7 @@ namespace Application.Email.Implementations
                 "</strong>" + "</FONT>" + "</center>" + "</div>" + "</header>" + "</section>" + "<br>" + "</br>" + "<section>" + "<div>" + "Señor(a): " + employerName + "<br>" +
                 "</br>" + "</div>" + "</section>" + "<section>" + "<div>" + "En este correo se incluye la lista de empleados que actualmente están excediendo el monto y/o " +
                 "la cantidad de beneficios de " + projectName + ". " + "<br>" + "</br>" + "</div> " + "</section>";
-            htmlContent += File.ReadAllText("../wwwroot/wwwroot/emails/OverstaffedEmployees.html");
+            htmlContent += File.ReadAllText("../Server_Planilla/wwwroot/emails/OverstaffedEmployees.html");
             
             htmlContent += "<section>" + "<div>" +"<br>" + "</br>" + "A cada uno de ellos se les envió un correo indicando que deben desuscribirse de alguno de sus beneficios." +
                 "</div> " + "</section>" + "<section>" + "<div>" + "Favor monitorear que cumplan con la fecha límite." + "<br>" + "</br>" + "</div>" + "</section>";
