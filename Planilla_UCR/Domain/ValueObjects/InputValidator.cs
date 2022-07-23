127f89b00227f0b36211c8e0311ec3014fc0127f
LeonelCamposM <leonelscamposm@gmail.com>
2022-07-22 20:08:05 -0600

    add Input validator

---
+++ Planilla_UCR/Domain/ValueObjects/InputValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    public class InputValidator
+    {
+        public bool ValidateStringSafety(string data)
+        {
+            bool error = true;
+            Regex queryPattern = new Regex(@"(?i)\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE|UNION( +ALL){0,1})\b");
+            Match queytValidaton = queryPattern.Match(data);
+            Regex injectionPattern = new Regex(@"((WHERE | OR)[ ] +[\(] *[ ] * ([\(] *[0 - 9] +[\)]*)[ ]*=[ ] *[\)]*[ ] *\3)| AND[ ] +[\(] *[ ] * ([\(] * 1[0 - 9] +|[2 - 9][0 - 9] *[\)]*)[ ] *[\(] *[ ] *=[ ] *[\)]*[ ] *\4");
+            Match injectionValidaton = queryPattern.Match(data);
+
+            if (!queytValidaton.Success || data.Length() > 255 || !injectionPattern.Success)
+            {
+                error = false;
+            }
+            return error;
+        }
+    }
+}
